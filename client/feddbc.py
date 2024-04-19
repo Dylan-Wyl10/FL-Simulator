a52e4c3955ab90f436c9bb77fdfd4fd79b1458f3
Dylan-Wyl10 <dylanwyl10@gmail.com>
2024-04-19 14:50:35 -0400

    add line 10-12

--- client/feddbc.py
+++ client/feddbc.py
@@ -2,11 +2,13 @@ import torch
 from .client import Client
 from utils import *
 from optimizer import *
-import numpy as np
+from random import random
 
 class fedavg(Client):
     def __init__(self, device, model_func, received_vecs, dataset, lr, args):
         super(fedavg, self).__init__(device, model_func, received_vecs, dataset, lr, args)
+        self.trigger_low = 0
+        self.trigger_high = 1
 
     def train(self):
         # local training
@@ -30,11 +32,15 @@ class fedavg(Client):
         last_state_params_list = get_mdl_params(self.model)
         # update parameter logic line 14-18
         """
-        Start with modifying the algorithm in algorith
+        Start with modifying the algorithm in algorithm line 10-18
         """
-        # 1. give bandwith, officially this is grabed randomly range 0-1
-        self.bandwith = np.random(0, 1)
+        # this is delta^{t}_{i}, line 10
         self.comm_vecs['local_update_list'] = last_state_params_list - self.received_vecs['Params_list']
+        # line 11: give bandwith, officially this is grabed randomly range 0-1
+        self.bandwith = random()
+        # line 12:
+        self.rou = self.bandwith * self.trigger_low + (1 - self.bandwith) * self.trigger_high
+
         self.comm_vecs['local_model_param_list'] = last_state_params_list
 
         return self.comm_vecs
